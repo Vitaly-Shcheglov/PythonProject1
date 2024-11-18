c5a9761516247fce0d83aedfc2f43fa7392c55b6
Vitaly Shcheglov <hallovit@yandex.ru>
2024-11-18 19:58:59 +0500

    save_homework_9.2

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,7 @@
-from src.masks import get_mask_card_number, get_mask_account
 import os
+
+from src.masks import get_mask_account, get_mask_card_number
+
 
 def mask_account_card(info: str) -> str:
     """Функция, которая маскирует номер карты или счета."""
@@ -7,7 +9,7 @@ def mask_account_card(info: str) -> str:
     if len(parts) < 2:
         return "Ошибка: некорректный ввод."
 
-    identifier = ' '.join(parts[:-1])
+    identifier = " ".join(parts[:-1])
     number = parts[-1]
 
     if identifier.lower().startswith("счет"):
@@ -17,11 +19,13 @@ def mask_account_card(info: str) -> str:
         masked_info = get_mask_card_number(number)
         return f"{identifier} {masked_info}"
 
+
 def get_date(date_str: str) -> str:
     """Функция, которая преобразует строку даты в нужный формат."""
     date_part = date_str.split("T")[0]
     year, month, day = date_part.split("-")
     return f"{day}.{month}.{year}"
+
 
 user_input = input("Введите тип и номер карты (или счет): ")
 masked_output = mask_account_card(user_input)
