be481a02c354a5ce2c0be929956a93f50f841939
Vitaly Shcheglov <hallovit@yandex.ru>
2024-12-02 19:15:26 +0500

    add test_widget

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -3,6 +3,7 @@ from src.masks import get_mask_card_number, get_mask_account
 
 @pytest.fixture
 def card_numbers():
+    """Фикстура, возвращающая список номеров карт для тестирования."""
     return [
         "7000792289606361",
         "1234567812345678",
@@ -14,6 +15,7 @@ def card_numbers():
 
 @pytest.fixture
 def account_numbers():
+    """Фикстура, возвращающая список номеров счетов для тестирования."""
     return [
         "73654108430135874305",
         "12345678901234567890",
@@ -28,24 +30,23 @@ def account_numbers():
     ("1234567812345678", "1234 56** **** 5678"),
 ])
 def test_get_mask_card_number_valid(card_number, expected):
+    """Тестирование правильности маскирования номера карты."""
     assert get_mask_card_number(card_number) == expected
 
 def test_get_mask_card_number_invalid(card_numbers):
+    """Тестирование обработки некорректных номеров карт."""
     for number in card_numbers[2:]:
         assert get_mask_card_number(number) == ""
-
 
 @pytest.mark.parametrize("account_number, expected", [
     ("73654108430135874305", "**4305"),
     ("12345678901234567890", "**7890"),
 ])
 def test_get_mask_account_valid(account_number, expected):
+    """Тестирование правильности маскирования номера счета."""
     assert get_mask_account(account_number) == expected
 
 def test_get_mask_account_invalid(account_numbers):
+    """Тестирование обработки некорректных номеров счетов."""
     for number in account_numbers[2:]:
         assert get_mask_account(number) == ""
-
-
-if __name__ == "__main__":
-    pytest.main()
